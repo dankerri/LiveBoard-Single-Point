a6d652bb470df0fbe4cde6317d0959a865adc24a
danker <itrrion@gmail.com>
2019-09-14 16:46:51 +0800

    修改了README.md

--- serverTest/sendCode.js
+++ serverTest/sendCode.js
@@ -1,7 +1,7 @@
 var io = require('socket.io-client')
 
 
-const ADDRESS = 'http://192.168.0.101';
+const ADDRESS = 'http://149.28.193.28';
 const PORT = 9999;
 
 const THE_URL = ADDRESS + ':' + PORT 
@@ -12,6 +12,6 @@ const socket = io(THE_URL)
 setInterval(() => {
     var myDate = new Date();
     var mytime=myDate.toLocaleTimeString();
-    socket.emit('newCode', {string: mytime})
+    socket.emit('newCode', {code : mytime})
 }, 1000)
 console.log("running")
